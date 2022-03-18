4313dfc3e39d58a45391db0a20592a93e85b12ab
kolyshek <kolyshek.posik@gmail.com>
2022-03-18 16:31:54 +0500

    Publication

---
+++ MainWindow.cpp
@@ -0,0 +1,14 @@
+#include "MainWindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , m_ui(new Ui::MainWindow)
+{
+    m_ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete m_ui;
+}
